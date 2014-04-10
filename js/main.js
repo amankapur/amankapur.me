8b9f5bb6913bdba042ef3f6a161f299f16213200
Aman Kapur <amankapur91@gmail.com>
2014-04-10 13:29:46 -0400

    calendar added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 $(function(){
-	$("#rails").on('click', function(e){
-		e.preventDefault();
-		console.log('hi');
-		$('#rails_projects').toggle('slow');
-	});
+	$(".calender").hide()
+	// $("#rails").on('click', function(e){
+	// 	e.preventDefault();
+	// 	console.log('hi');
+	// 	$('#rails_projects').toggle('slow');
+	// });
+	
+
+	var hash = window.location.hash;
+	console.log(hash);
+	if (hash.indexOf("calendar") > 0){
+		$(".calender").show();
+		$(".mainrow").hide();
+		console.log("showing calender");
+	}
+	else {
+		$(".calender").hide();
+		$(".mainrow").show();
+	}
+	
+
 });	
 
